349c8c2dd1b323c1af4caa8d0c4847812e1d7913
ffons-ti <ffons-ti@student.42madrid.com>
2023-03-15 14:43:17 +0100

    norm

--- ft_free_matrix.c
+++ ft_free_matrix.c
@@ -6,12 +6,11 @@
 /*   By: ffons-ti <ffons-ti@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 12:23:06 by ffons-ti          #+#    #+#             */
-/*   Updated: 2023/03/09 11:43:29 by ffons-ti         ###   ########.fr       */
+/*   Updated: 2023/03/14 13:17:17 by ffons-ti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
 
 void	ft_free_matrix(void **matrix)
 {
